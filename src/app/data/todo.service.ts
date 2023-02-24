1cedd988c8eca0414ada8c9d0be264a5cc874eb5
Aziz <aziznal.dev@gmail.com>
2023-02-24 11:46:58 +0300

    convert service to use events

--- src/app/data/todo.service.ts
+++ src/app/data/todo.service.ts
@@ -1,44 +1,113 @@
 import { Todo } from '../models/todo.model';
 import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject, delay, mergeMap, Observable, of, Subject, switchMap, tap } from 'rxjs';
-import { moveItemInArray } from '@angular/cdk/drag-drop';
+import { BehaviorSubject, Observable, Subject, throwError } from 'rxjs';
 import { TodoApiService } from './todo-api';
+
+interface TodoServiceState {
+  data?: Todo[] | null;
+  loading: boolean;
+  error?: string | null;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService implements OnDestroy {
-  #todoItems$: Subject<Todo[]> = new Subject();
-
-  // whether an async operation is in progress
-  loading$ = new BehaviorSubject(true);
+  state$ = new BehaviorSubject<TodoServiceState>({
+    data: null,
+    loading: false,
+    error: null,
+  });
 
   constructor(private todoApi: TodoApiService) {
     this.#loadInitialData();
   }
 
-  get todos$(): Observable<Todo[]> {
-    return this.#todoItems$;
+  get todos$(): Observable<TodoServiceState> {
+    return this.state$;
   }
 
-  async addTodoItem(todoItem: Todo): Promise<void> {}
+  async addTodoItem(todoItem: Todo): Promise<void> {
+    this.state$.next({ ...this.state$.value, loading: true });
 
-  async updateTodoItem(todoItem: Todo): Promise<void> {}
+    try {
+      await this.todoApi.addTodoItem(todoItem);
 
-  async deleteTodoItem(id: number): Promise<void> {}
+      await this.#refresh();
+    } catch (error) {
+      this.emit({ error: 'Something went wrong while adding TODO!' });
+    } finally {
+      this.emit({ loading: false });
+    }
+  }
 
-  async reorderItem(fromIndex: number, toIndex: number): Promise<void> {}
+  async updateTodoItem(todoItem: Todo): Promise<void> {
+    this.emit({ loading: true });
+
+    try {
+      await this.todoApi.updateTodoItem(todoItem);
+
+      await this.#refresh();
+    } catch (error) {
+      this.emit({ error: 'Something went wrong while updating TODO!' });
+    } finally {
+      this.emit({ loading: false });
+    }
+  }
+
+  async deleteTodoItem(id: number): Promise<void> {
+    this.emit({ loading: true });
+
+    try {
+      await this.todoApi.deleteTodoItem(id);
+
+      await this.#refresh();
+    } catch (error) {
+      this.emit({ error: 'Something went wrong while deleting TODO!' });
+    } finally {
+      this.emit({ loading: false });
+    }
+  }
+
+  async reorderItem(fromIndex: number, toIndex: number): Promise<void> {
+    this.emit({ loading: true });
+
+    try {
+      await this.todoApi.reorderItem(fromIndex, toIndex);
+
+      const data = await this.todoApi.getTodoItems();
+      this.emit({ data });
+    } catch (error) {
+      this.emit({ error: 'Something went wrong while reordering TODOs!' });
+    } finally {
+      this.emit({ loading: false });
+    }
+  }
 
   async #loadInitialData() {
-    this.loading$.next(true);
+    this.emit({ loading: true });
 
-    this.#todoItems$.next(await this.todoApi.getTodoItems());
+    try {
+      await this.#refresh();
+    } catch (error) {
+      this.emit({ error: 'Something went wrong while loading TODOs!' });
+    } finally {
+      this.emit({ loading: false });
+    }
+  }
 
-    this.loading$.next(false);
+  // method to emit state
+  emit(state: Partial<TodoServiceState>): void {
+    this.state$.next({ ...this.state$.value, ...state });
+  }
+
+  async #refresh(): Promise<void> {
+    const data = await this.todoApi.getTodoItems();
+
+    this.emit({ data });
   }
 
   ngOnDestroy(): void {
-    this.#todoItems$.complete();
-    this.loading$.complete();
+    this.state$.complete();
   }
 }
